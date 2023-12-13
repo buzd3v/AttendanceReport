bb6382df6a8a667312f85e2f2e52145957b818ff
Kien Nguyen <kanashkaze02@gmail.com>
2023-12-14 05:36:53 +0700

    fixing DasboardController

--- src/main/java/demo/demo/DashboardController.java
+++ src/main/java/demo/demo/DashboardController.java
@@ -83,8 +83,8 @@ public class DashboardController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        initPanes();
-        paneHome.toFront();
+//        initPanes();
+//        paneHome.toFront();
     }
     private void setAnchorConstraint(BorderPane borderPane) {
 
